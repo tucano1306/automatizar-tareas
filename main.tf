969a6456eb3933e9b175a83c2aaf0e8cb9a328c7
Leonardo <66296967+tucano1306@users.noreply.github.com>
2024-08-16 19:29:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
+  name = "ec2tarea_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
